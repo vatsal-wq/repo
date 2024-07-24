7ee127bb7852676b90aaf826556d5853c7a6cb8e
vatsal-wq <51950758+vatsal-wq@users.noreply.github.com>
2024-07-24 23:52:42 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private double price;
+
+    public Book(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Price: $" + price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Double.compare(book.price, price) == 0 && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, price);
+    }
+}
