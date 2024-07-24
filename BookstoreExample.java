a4831ded68217de7cd43c25100ef1fd90f33fd33
vatsal-wq <51950758+vatsal-wq@users.noreply.github.com>
2024-07-24 23:56:19 +0530

    Update BookstoreExample.java

--- BookstoreExample.java
+++ BookstoreExample.java
@@ -7,25 +7,25 @@ public class BookstoreExample {
         Bookstore bookstore = new Bookstore();
 
         // Add 10 books to the inventory
-        bookstore.addBook(new Book("Book 1", 10.99));
-        bookstore.addBook(new Book("Book 2", 12.99));
-        bookstore.addBook(new Book("Book 3", 8.99));
-        bookstore.addBook(new Book("Book 4", 15.99));
-        bookstore.addBook(new Book("Book 5", 6.99));
-        bookstore.addBook(new Book("Book 6", 9.99));
-        bookstore.addBook(new Book("Book 7", 11.99));
-        bookstore.addBook(new Book("Book 8", 7.99));
-        bookstore.addBook(new Book("Book 9", 14.99));
-        bookstore.addBook(new Book("Book 10", 13.99));
+        bookstore.addBook(new Book("1234567890", "Book 1", 10.99));
+        bookstore.addBook(new Book("2345678901", "Book 2", 12.99));
+        bookstore.addBook(new Book("3456789012", "Book 3", 8.99));
+        bookstore.addBook(new Book("4567890123", "Book 4", 15.99));
+        bookstore.addBook(new Book("5678901234", "Book 5", 6.99));
+        bookstore.addBook(new Book("6789012345", "Book 6", 9.99));
+        bookstore.addBook(new Book("7890123456", "Book 7", 11.99));
+        bookstore.addBook(new Book("8901234567", "Book 8", 7.99));
+        bookstore.addBook(new Book("9012345678", "Book 9", 14.99));
+        bookstore.addBook(new Book("0123456789", "Book 10", 13.99));
 
         // Display the initial inventory
         logger.info("Initial Inventory:");
         bookstore.displayInventory();
 
         // Remove some books from the bookstore
-        bookstore.removeBook("Book 3");
-        bookstore.removeBook("Book 7");
-        bookstore.removeBook("Book 10");
+        bookstore.removeBook("3456789012");
+        bookstore.removeBook("7890123456");
+        bookstore.removeBook("0123456789");
 
         // Display the updated inventory after removal
         logger.info("Updated Inventory:");
